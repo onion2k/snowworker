a37531011850dcc113b6ea932334ee3fb417782f
Chris <chris@usablehq.com>
2017-11-05 17:12:20 +0000

    Basic snowfall

--- src/snowfall.js
+++ src/snowfall.js
@@ -29,15 +29,33 @@ const map = () => {
         let bounds = snowEl.getClientRects();
         platforms.push({ left: bounds[0].left, width: bounds[0].width, top: bounds[0].top });
     });    
-
-    // draw();
     
+}
+
+const snowflakes = [];
+
+for (var i=0; i<300; i++) {
+    var x = Math.random() * snowCanvas.width;
+    var y = Math.random() * snowCanvas.height;
+    snowflakes.push({ x: x, y: y, vx: 4, vy: 1 + (Math.random()*2), l: 3000 });
 }
 
 const draw = () => {
     ctx.clearRect(0, 0, snowCanvas.width, snowCanvas.height);
-    platforms.forEach((platform) => {
-        ctx.fillRect(platform.left, platform.top-10-document.body.scrollTop, platform.width, 10);
+    ctx.fillStyle = 'white';
+    snowflakes.forEach((f) => {
+        ctx.beginPath();
+        ctx.arc(f.x, f.y-document.body.scrollTop, 3, 0, 2 * Math.PI, false);
+        ctx.fill();
+        f.x += Math.random()*f.vx - (f.vx/2);
+        f.y += f.vy;
+        if (f.y>snowCanvas.height) { f.y = 0; f.x = Math.random()*snowCanvas.width; f.l=3000; }
+        platforms.forEach((platform) => {
+            if ( (f.y > platform.top-3 && f.y < platform.top) && (f.x > platform.left && f.x < platform.left+platform.width) ) {
+                f.vy = f.vx = 0;
+            }
+        });
+        if (--f.l === 0) { f.y = 0; f.x = Math.random()*snowCanvas.width; f.vx = 2; f.vy = 1 + (Math.random()*2); f.l=3000; }
     });
     requestAnimationFrame(draw);
 }
@@ -50,7 +68,6 @@ var myEfficientFn = debounce(function() {
 }, 16);
 
 window.addEventListener('resize', myEfficientFn);
-// window.addEventListener('scroll', draw);
 
 function debounce(func, wait, immediate) {
 	var timeout;
