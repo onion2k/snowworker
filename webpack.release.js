605ac42201421d3d8ede9a2bf37e758feec76077
Chris <chris@usablehq.com>
2017-11-11 12:10:45 +0000

    Added build and release configs (need to be refactored)

---
+++ webpack.release.js
@@ -0,0 +1,62 @@
+var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SriPlugin = require ('webpack-subresource-integrity');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+    entry: {
+        app: ["./src/snowfall.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/",
+        filename: "snowfall.js",
+        crossOriginLoading: "anonymous"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "assets"),
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(glsl|frag|vert|html)$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new WebpackCleanupPlugin(),
+        new CopyWebpackPlugin([
+            { from: 'assets' }
+        ]),
+        new UglifyJSPlugin(),
+        new HtmlWebpackPlugin({ template: 'assets/index.html' }),
+        new ExtractTextPlugin("snowfall.css"),
+    ]
+};
+
+config.plugins.push(
+    new SriPlugin({
+        hashFuncNames: ['sha256', 'sha384'],
+        enabled: true
+    })
+);
+
+config.plugins.push(
+    new CopyWebpackPlugin([
+        { from: 'build/snowfall.js', to: '../dist/snowfall.js' }
+    ])
+)
+
+module.exports = config;
