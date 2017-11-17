5f6740f92c4513105cf024824114fa8f75317a7b
Chris Neale <chris@komododigital.co.uk>
2017-11-17 08:55:53 +0000

    Removed dead code

--- src/snowfall.js
+++ src/snowfall.js
@@ -55,51 +55,16 @@ const screenMap = () => {
         let bounds = rooftopEl.getClientRects();
         platforms.push({ left: bounds[0].left, width: bounds[0].width, top: bounds[0].top });
     });
-    // snowflakesStatic.forEach((f,i) => {
-    //     let keep = false;
-    //     platforms.forEach((platform) => {
-    //         if ( (f.x > platform.left && f.x < platform.left+platform.width) ) {
-    //             keep = true;
-    //         }
-    //     });    
-    //     if (!keep) { delete snowflakesStatic[i]; }
-    // });
 }
 
 const draw = (snowflakes) => {
 
     ctx.clearRect(0, 0, snowCanvas.width, snowCanvas.height);
 
-    // ctx.fillStyle = 'white';
-    // snowflakesStatic.forEach((f, i) => {
-    //     ctx.beginPath();
-    //     ctx.arc(f.x, f.y-document.body.scrollTop, f.s, 0, 2 * Math.PI, false);
-    //     ctx.fill();
-    //     if (f.l--===0) {
-    //         delete snowflakesStatic[i];
-    //     }
-    // });
-
     snowflakes.forEach((f) => {
         ctx.beginPath();
         ctx.arc(f.x, f.y-document.body.scrollTop, f.s, 0, 2 * Math.PI, false);
         ctx.fill();
-        // f.x += (Math.random()*(f.vx/deltaDistance) - ((f.vx/deltaDistance)/2)) * timefactor * deltaCorrection;
-        // f.y += (f.vy/deltaDistance) * timefactor * deltaCorrection;
-        // if (f.y>pageHeight) {
-        //     f.y = 0;
-        //     f.x = Math.random()*snowCanvas.width;
-        // } else {
-        //     platforms.forEach((platform) => {
-        //         if ( (f.y > platform.top-3 && f.y < platform.top) && (f.x > platform.left && f.x < platform.left+platform.width) && Math.floor(Math.random()*2)%2==0 ) {
-        //             // snowflakesStatic.push({ x: f.x, y: f.y, l: snowflakesLifetime, s: f.s });
-        //             f.y = 0;
-        //             f.x = Math.random()*snowCanvas.width;
-        //             f.vx = 80;
-        //             f.vy = 40 + (Math.random()*60);
-        //         }
-        //     });    
-        // }
     });
 
     prevTimestamp = timestamp;
@@ -109,4 +74,3 @@ const draw = (snowflakes) => {
 window.addEventListener('resize', screenMap);
 
 screenMap();
-// draw();
